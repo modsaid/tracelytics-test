f8e4397c64627b72c8c0751d3a2785210d7dfb85
Mahmoud Said <mahmoud@modsaid.com>
2012-11-22 13:55:28 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tracelyticstest::Application.config.secret_token = 'dbb6fac2267cb7cd0e89acdf396a23d179094d04b5392027c8dcfb5e866e6234f453e64df6ee65596ee71128d74a21b5e6048ca3504053b2d7482a2851391bcb'
