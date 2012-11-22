f97b77f75ca89c512efa394a1e5cb767690073e3
Mahmoud Said <mahmoud@modsaid.com>
2012-11-22 19:26:34 +0200

    setting starting point for codes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,24 +1,34 @@
 class Product < ActiveRecord::Base
 
-	def self.generate_test_data(size=10000, batch=1000)
+	def self.generate_test_data(size=10000, batch=1000, start=0)
     products =[]
-    size.to_i.times do |i| 
+    size=size.to_i
+    start=start.to_i
+    start.upto(start + size - 1) do |i| 
       products ||=[]
       attrs ={:code=> "#{i}_P_#{i}", :name => "product #{i}", :category_id => i % 20}
       products << attrs
 
       if (i % batch).zero? || i==(size-1)
-        Product.transaction do 
-          products.each do |attrs|
-            Product.create(attrs)
-          end
-          print "."
-          products.clear
-        end
+        flush(products)
       end
 
     end
+
+    flush(products) unless products.empty?
+
     puts ""
     puts "Data size: #{Product.count}"
+  end
+
+  private
+  def self.flush(products)
+    Product.transaction do 
+      products.each do |attrs|
+        Product.create(attrs)
+      end
+      print "."
+      products.clear
+    end
   end
 end
--- lib/tasks/generate_test_data.rake
+++ lib/tasks/generate_test_data.rake
@@ -2,12 +2,20 @@ namespace :tracelytics_test do
 
   DEFAULT_SIZE=10000
   DEFAULT_BATCH=1000
+  DEFAULT_START=0
   desc "Generate test data to fill the database takes SIZE [#{DEFAULT_SIZE}] and BATCH [#{DEFAULT_BATCH}] arguments"
   task :generate_test_data => :environment do 
     size = (ENV['SIZE'] || DEFAULT_SIZE).to_i
     batch = (ENV['BATCH'] || DEFAULT_BATCH).to_i
+    start = (ENV['START'] || DEFAULT_START).to_i
 
     puts "generating with SIZE=#{size}, BATCH=#{batch}"
-    Product.generate_test_data(size, batch)
+    Product.generate_test_data(size, batch, start)
+  end
+
+  desc "Generate codes file"
+  task :generate_codes_file do 
+    max=5000000
+    File.open('keys.txt','w'){|f| max.times{ i=rand(max); f.puts "#{i}_P_#{i}" }};''
   end
 end
